cb0d24351701376bc80f3960bbb0f26cb73e52cf
madison-mitchell <missmadisonrose@icloud.com>
2023-06-10 15:27:58 -0400

    Update Resume Download Button

--- src/components/ResumeDownload.vue
+++ src/components/ResumeDownload.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="online-resume flex flex-col w-full bg-neutral-50 items-center justify-center">
         <h1 class="text-3xl font-bold mt-10">Online Resume</h1>
-        <a href="/Madison Mitchell Resume.pdf" download>
+        <a href="/Madison%20Mitchell%20Resume.pdf" download>
             <button class="bg-sky-500 text-white font-bold mt-4 mb-10 w-fit px-4 py-2 rounded"><i class="fa-solid fa-file-lines pr-2"></i>Download PDF Version</button>
         </a>
     </header>
